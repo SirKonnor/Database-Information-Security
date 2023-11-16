788508e9b4b53f9dcafd8b62dc2ab53b6caeedfd
SirKonnor <144115654+SirKonnor@users.noreply.github.com>
2023-11-16 11:21:06 +0300

    Update python.py

--- python.py
+++ python.py
@@ -128,3 +128,8 @@ else:
     print('YES')
 
 Евклидово расстояние
+x1 = float(input())
+y1 = float(input())
+x2 = float(input())
+y2 = float(input())
+print((((x1 - x2) ** 2) + ((y1 - y2) ** 2)) ** 0.5)
