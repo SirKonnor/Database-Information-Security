39cd5e04c54f1b44c2124bc2e0e29f7dcad1f7b7
SirKonnor <144115654+SirKonnor@users.noreply.github.com>
2023-11-16 11:29:49 +0300

    Update python.py

--- python.py
+++ python.py
@@ -133,3 +133,10 @@ y1 = float(input())
 x2 = float(input())
 y2 = float(input())
 print((((x1 - x2) ** 2) + ((y1 - y2) ** 2)) ** 0.5)
+
+Площадь и длина
+r = float(input())
+import math
+print(math.pi * r ** 2)
+print(2 * math.pi * r)
+
